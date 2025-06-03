0c89e5112298af09a4b9f7013c681edb931215f3
Hung Pham <hupham@axon.com>
2025-06-03 15:44:37 +0700

    Update script

--- setup.sh
+++ setup.sh
@@ -70,11 +70,11 @@ echo "🚀 Deploying whoami service to test the setup..."
 microk8s kubectl apply -f $SCRIPT_DIR/whoami/whoami.yaml
 microk8s kubectl rollout status deployment/whoami
 
-
-
 cp "$SCRIPT_DIR/whoami/whoami.yaml" ~/whoami.yaml
 host=$(yq 'select(.kind == "Ingress") | .spec.tls[0].hosts[0]' ~/whoami.yaml)
 rm ~/whoami.yaml
 echo "🔗 You can now access the whoami service at https://$host"
+echo "To undeploy the whoami service, run:"
+echo "microk8s kubectl delete -f $SCRIPT_DIR/whoami/whoami.yaml"
 
 echo "✅ MicroK8s setup complete!"

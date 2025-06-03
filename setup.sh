4883ddd5a40b6a495274e97b60d0d60703831aca
Hung Pham <hupham@axon.com>
2025-06-03 16:03:24 +0700

    Update setup.sh script

--- setup.sh
+++ setup.sh
@@ -66,15 +66,6 @@ echo "🔧 Configuring cert-manager ClusterIssuer..."
 create_cluster_issuer "letsencrypt-prod" "$SCRIPT_DIR/cert-manager/cluster-issuer-prod.yaml"
 
 # Deploy whoami service to test the setup
-echo "🚀 Deploying whoami service to test the setup..."
-microk8s kubectl apply -f $SCRIPT_DIR/whoami/whoami.yaml
-microk8s kubectl rollout status deployment/whoami
-
-cp "$SCRIPT_DIR/whoami/whoami.yaml" ~/whoami.yaml
-host=$(yq 'select(.kind == "Ingress") | .spec.tls[0].hosts[0]' ~/whoami.yaml)
-rm ~/whoami.yaml
-echo "🔗 You can now access the whoami service at https://$host"
-echo "To undeploy the whoami service, run:"
-echo "microk8s kubectl delete -f $SCRIPT_DIR/whoami/whoami.yaml"
+"$SCRIPT_DIR/clean.sh"
 
 echo "✅ MicroK8s setup complete!"

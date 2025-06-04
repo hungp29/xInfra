2755fd70f8c0dfaf6ebe21a9fe10a7ab1cd154b0
Hung Pham <hupham@axon.com>
2025-06-04 16:35:44 +0700

    Update script undeploy postgres

--- undeploy/postgres/undeploy.sh
+++ undeploy/postgres/undeploy.sh
@@ -25,12 +25,13 @@ microk8s kubectl delete secret "$POSTGRES_SECRET_NAME" --namespace "$INFRA_NAMES
 PVC_NAME=$(microk8s kubectl get pvc -n "$INFRA_NAMESPACE" \
   -l app.kubernetes.io/instance="$POSTGRES_RELEASE_NAME" \
   -o jsonpath="{.items[0].metadata.name}" 2>/dev/null || true)
-
+echo "📦 Deleting PVC: $PVC_NAME"
 echo "📦 Deleting PVCs..."
 microk8s kubectl delete pvc -l app.kubernetes.io/instance="$POSTGRES_RELEASE_NAME" --namespace "$INFRA_NAMESPACE" || true
 
 PV_NAME=$(microk8s kubectl get pvc "$PVC_NAME" -n "$INFRA_NAMESPACE" \
   -o jsonpath="{.spec.volumeName}" 2>/dev/null || true)
+echo "📦 Deleting PV: $PV_NAME"
 if [[ ! -z "$PV_NAME" ]]; then
   echo "Consider deleting the pv manually: '$PV_NAME'"
   microk8s kubectl describe pv "$PV_NAME"

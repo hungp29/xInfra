276b87280939e0df26dfbe3ca7ad65cdd41ae7e4
Hung Pham <hupham@axon.com>
2025-06-12 09:52:03 +0700

    Add script check_network.sh

--- scripts/check_network.sh
+++ scripts/check_network.sh
@@ -8,13 +8,13 @@ if ! ping -c 3 "$TARGET" > /dev/null 2>&1; then
     echo "$(date): Network unreachable. Restarting network..." >> /var/log/check-network.log
 
     # Try to restart the network service
-    systemctl restart systemd-networkd #|| systemctl restart NetworkManager
+    sudo systemctl restart systemd-networkd #|| systemctl restart NetworkManager
 
     # If the network is still unreachable, reboot the system
     sleep 60
     if ! ping -c 3 "$TARGET" > /dev/null 2>&1; then
         echo "$(date): Still no network. Rebooting..." >> /var/log/check-network.log
-        reboot
+        sudo reboot
     fi
 else
     echo "$(date): Network OK." >> /var/log/check-network.log
